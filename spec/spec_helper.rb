ec3f9b8a099dd08bc90f06e6f9bfd5a55dcb4620
Yuji Yokoo <yujiyokoo@gmail.com>
2016-06-21 17:18:59 +0000

    Add search form and simple index view spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,4 +92,11 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.around(:each, type: :view) do |ex|
+    config.mock_with :rspec do |mocks|
+      mocks.verify_partial_doubles = false
+      ex.run
+      mocks.verify_partial_doubles = true
+    end
+  end
 end
---
+++ spec/views/flickr_images/index.html.erb_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'flickr_images/index.html.erb' do
+  before do
+    allow(view).to receive(:flickr_images) { [] }
+    allow(view).to receive(:search_form) { FlickrSearchForm.new }
+  end
+
+  it 'shows a text field in a search form' do
+    render
+
+    expect(rendered).to have_selector('form input[type=text]')
+  end
+
+  it 'shows a submit button in a search form' do
+    render
+
+    expect(rendered).to have_selector('form input[type=submit]')
+  end
+
+  context 'with images' do
+    let(:image) { double('image', url: 'image.jpg', title: 'image title') }
+
+    before do
+      allow(view).to receive(:flickr_images) { [image] }
+    end
+
+    it 'shows title' do
+      render
+
+      expect(rendered).to have_content('image title')
+    end
+
+    it 'shows image with the image url' do
+      render
+
+      expect(rendered).to have_selector('img[src$="image.jpg"]')
+    end
+  end
+end
